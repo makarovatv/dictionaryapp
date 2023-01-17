4f5d54756dd4a84e14cd6584d39426c52b024587
makarovatv <makarovatv80@gmail.com>
2023-01-17 21:10:52 +0200

    made searching form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from "./logo.png"
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo img-fluid"
-          alt="logo"
-        />
-      </header>
+      <div className="App-header">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="App-footer">
+            <small>Coded by Tanya Makarova</small>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
