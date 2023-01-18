134365c02e0befd3dd26a28b957160185a114c0a
makarovatv <makarovatv80@gmail.com>
2023-01-18 22:02:31 +0200

    Added meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import "./Dictionary.css";
 import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data);
+        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
   
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         console.log(apiUrl);
         axios.get(apiUrl).then(handleResponse);
     }
@@ -23,5 +27,6 @@ function handleKeywordChange(event) {
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange} />
         </form>
+        <Results results={results} />
     </div>;
 }
