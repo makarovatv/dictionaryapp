5a79027a95c872e8abf47ba9a3fbca049615e1e4
makarovatv <makarovatv80@gmail.com>
2023-01-18 12:35:02 +0200

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    function handleResponse(response) {
+        console.log(response.data);
+    }
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} defenition`);
+  
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleResponse);
     }
 function handleKeywordChange(event) {
     setKeyword(event.target.value);
